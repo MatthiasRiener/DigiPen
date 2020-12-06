8056ff5c3ff4407b11875ebefb522c7575d2d654
Jonas Himmetsberger <j.himmetsberger2@gmail.com>
2020-12-06 11:25:53 +0100

    editor dragging update

---
+++ src/editor/dragging/js/eventhandler.js
@@ -0,0 +1,10 @@
+// This is where all events are 
+
+window.onload = function () {
+    console.log('Document loaded.');
+    // resize();
+}
+
+// function resize() {
+//     document.getElementsByClassName('upper-canvas lower-canvas')
+// }
