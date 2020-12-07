3139d4df042b219229c515d87b2bba357a3d7e5f
LukasFriesenecker <friesilukas@gmail.com>
2020-12-07 14:16:20 +0100

    closes #12

---
+++ src/editor/view/js/eventhandler.js
@@ -0,0 +1,35 @@
+let trackingIndex = 0;
+
+$(document).ready(function () {
+    console.log('Document loaded.');
+    addSlide();
+    toggleVisibility(0);
+});
+
+$('#content-leftSlides-topBar').click(function(){
+    addSlide();
+});
+
+function addSlide(){
+    let slides = $('.content-leftSlides-slidesContent-slide');
+    let width = 10;
+    let height = width / (16 / 9);
+
+    $('#content-leftSlides-slidesContent').append(`
+    <div class="content-leftSlides-slidesContent-slide">
+        <div class="content-leftSlides-slidesContent-slide-leftBar ${slides.length == 0 ? 'activeSlide' : ''}" style="height: ${height}vw;"></div>
+        <div class="content-leftSlides-slidesContent-slide-middleBar" style="height: ${height}vw;">${slides.length + 1}</div>
+        <div class="content-leftSlides-slidesContent-slide-content" style="height: ${height}vw; ${slides.length == 0 ? 'transform: scale(0.95);' : ''}" onclick="toggleVisibility(${slides.length})"></div>
+    </div>`);
+
+    $('#content-leftSlides-slidesContent-animatedBar').height(height + 'vw');
+}
+
+function toggleVisibility(index){
+    trackingIndex = index;
+    $('#content-leftSlides-slidesContent-animatedBar').css({'top':`${$("#content-leftSlides-slidesContent-animatedBar").position().top}`}).animate({"top":`${$('.content-leftSlides-slidesContent-slide-leftBar').eq(trackingIndex).position().top}px`}, "slow");
+}
+
+$('#content-leftSlides-slidesContent').scroll(function(){
+    $('#content-leftSlides-slidesContent-animatedBar').css("top", `${$('.content-leftSlides-slidesContent-slide-leftBar').eq(trackingIndex).position().top}px`);
+});
