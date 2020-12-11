4a0453c99bbba425a201f7fc6c0212d54f696bdf
Matthias Riener <matti.riener@gmail.com>
2020-12-11 16:40:09 +0100

    finished terms of service

--- src/terms_of_service/js/eventhandler.js
+++ src/terms_of_service/js/eventhandler.js
@@ -1,5 +1,28 @@
 // This is where all events are 
 
 window.onload = function () {
-    console.log('Document loaded.')
+   loadTerms();
 }
+
+function loadTerms() {
+    $.get("https://raw.githubusercontent.com/MatthiasRiener/Slidea/main/src/terms_of_service/terms/terms.md", function(data) {
+        var terms = data.split("\n").filter(Boolean); 
+        console.log(terms)
+
+        terms.forEach(element => {
+            if(element.startsWith("#")) {
+                $('.side-bar').eq(0).append(`<div class="content-point" ><p>${element.slice(1, element.length).trim()}</p></div>`);
+                $('.main-content').eq(0).append(`<p class="title" id="${element.slice(1, element.length).trim().replaceAll(" ", "_")}">${element.slice(1, element.length).trim()}</div>`);
+            } else {
+                $('.main-content').eq(0).append(`<p class="content">${element.trim()}</div>`);
+            }
+        });
+    });
+}
+
+$('body').on('click', '.content-point ', function() {
+    console.log($(this).children("p").text().trim().replaceAll(" ", "_"));
+    $(".main-content").animate({scrollTop: (document.getElementsByClassName('main-content')[0].scrollTop + $(`#${$(this).children("p").text().trim().replaceAll(" ", "_")}`).position().top)}, 800, 'swing');
+
+    //document.getElementById(`${$(this).children("p").text().trim().replaceAll(" ", "_")}`).scrollIntoView({behavior: 'smooth'});
+});
