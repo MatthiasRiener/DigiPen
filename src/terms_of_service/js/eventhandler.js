395911c921ad947261d784aee43be519787eef7a
Matthias Riener <matti.riener@gmail.com>
2020-12-11 16:42:05 +0100

    finished page, removed unused parts

--- src/terms_of_service/js/eventhandler.js
+++ src/terms_of_service/js/eventhandler.js
@@ -1,16 +1,15 @@
 // This is where all events are 
 
 window.onload = function () {
-   loadTerms();
+    loadTerms();
 }
 
 function loadTerms() {
-    $.get("https://raw.githubusercontent.com/MatthiasRiener/Slidea/main/src/terms_of_service/terms/terms.md", function(data) {
-        var terms = data.split("\n").filter(Boolean); 
-        console.log(terms)
+    $.get("https://raw.githubusercontent.com/MatthiasRiener/Slidea/main/src/terms_of_service/terms/terms.md", function (data) {
+        var terms = data.split("\n").filter(Boolean);
 
         terms.forEach(element => {
-            if(element.startsWith("#")) {
+            if (element.startsWith("#")) {
                 $('.side-bar').eq(0).append(`<div class="content-point" ><p>${element.slice(1, element.length).trim()}</p></div>`);
                 $('.main-content').eq(0).append(`<p class="title" id="${element.slice(1, element.length).trim().replaceAll(" ", "_")}">${element.slice(1, element.length).trim()}</div>`);
             } else {
@@ -20,9 +19,10 @@ function loadTerms() {
     });
 }
 
-$('body').on('click', '.content-point ', function() {
-    console.log($(this).children("p").text().trim().replaceAll(" ", "_"));
-    $(".main-content").animate({scrollTop: (document.getElementsByClassName('main-content')[0].scrollTop + $(`#${$(this).children("p").text().trim().replaceAll(" ", "_")}`).position().top)}, 800, 'swing');
+$('body').on('click', '.content-point ', function () {
+    $(".main-content").animate({
+        scrollTop: (document.getElementsByClassName('main-content')[0].scrollTop + $(`#${$(this).children("p").text().trim().replaceAll(" ", "_")}`).position().top)
+    }, 800, 'swing');
 
     //document.getElementById(`${$(this).children("p").text().trim().replaceAll(" ", "_")}`).scrollIntoView({behavior: 'smooth'});
 });
