708d3aebb60ed74621b3b96f94cd57c7c788f216
Matthias Riener <matti.riener@gmail.com>
2020-12-18 20:34:53 +0100

    started task-adding date calc

--- src/task-management/js/eventhandler.js
+++ src/task-management/js/eventhandler.js
@@ -4,6 +4,7 @@ window.onload = function () {
     console.log('Document loaded.')
     loadCalendar()
     insertProfileImages()
+    calculateTaskWidth();
 }
 
 
@@ -22,7 +23,7 @@ function loadCalendar() {
 
     for(let i = 1; i <= 18; i++) {
         let newDate = new Date(now.setDate(now.getDate() + 1));
-        $('.calendar-row').append(`<div class="calendar-day"><p>${newDate.getDate()}</p></div>`)
+        $('.calendar-row').append(`<div class="calendar-day" data-date="${newDate}" ><p>${newDate.getDate()}</p></div>`)
     }
 
     $('.calendar-row').eq(0).append(` <div class="spacing-calendar">
@@ -35,11 +36,30 @@ function insertProfileImages() {
         const number = randomNumber(2, 5);
         console.log(number)
         for(let i = 0; i < number; i++) {
-            $('.profile-images').eq(index).append(`<div style="margin-left: -0.4vw; z-index: ${number - i}" class="user"></div>`)
+            $('.profile-images').eq(index).append(`<div style="margin-left: -0.4vw; z-index: ${number - i}; background-image: url(../img/user_${randomNumber(1,14)}.png)" class="user"></div>`)
         }
     });
 }
 
 function randomNumber(min, max) {
     return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+
+
+function calculateTaskWidth(start, end) {
+    // Calculate Width of Single Container (Day)
+    var distance = $('.calendar-day').width();
+    console.log(distance);
+}
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+// a and b are javascript Date objects
+function dateDiffInDays(a, b) {
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
 }
