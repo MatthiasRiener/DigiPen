7ffcd5ac8475b6ad602ee87cf1ed9149f7c12243
Matthias Riener <matti.riener@gmail.com>
2020-12-18 20:16:24 +0100

    automatic generate of task header

--- src/task-management/js/eventhandler.js
+++ src/task-management/js/eventhandler.js
@@ -2,29 +2,44 @@
 
 window.onload = function () {
     console.log('Document loaded.')
-    loadCalendar();
+    loadCalendar()
+    insertProfileImages()
 }
 
 
 $('#change-visibility').click(function() {
-    $('.visibility-popup').toggleClass("visibility-class");
+    $('.visibility-popup').toggleClass("visibility-class")
 });
 
 
 function loadCalendar() {
-    let now = new Date();
-    now.setDate(now.getDate() - 5);
+    let now = new Date()
+    now.setDate(now.getDate() - 5)
    
     $('.calendar-row').eq(0).append(` <div class="spacing-calendar">
 
-    </div>`);
+    </div>`)
 
     for(let i = 1; i <= 18; i++) {
         let newDate = new Date(now.setDate(now.getDate() + 1));
-        $('.calendar-row').append(`<div class="calendar-day"><p>${newDate.getDate()}</p></div>`);
+        $('.calendar-row').append(`<div class="calendar-day"><p>${newDate.getDate()}</p></div>`)
     }
 
     $('.calendar-row').eq(0).append(` <div class="spacing-calendar">
 
-    </div>`);
+    </div>`)
+}
+
+function insertProfileImages() {
+    $('.profile-images').each(function(index, obj) {
+        const number = randomNumber(2, 5);
+        console.log(number)
+        for(let i = 0; i < number; i++) {
+            $('.profile-images').eq(index).append(`<div style="margin-left: -0.4vw; z-index: ${number - i}" class="user"></div>`)
+        }
+    });
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min)
 }
