2313091a38110bbbd3ba683560b18cde5d58d32a
Matthias Riener <matti.riener@gmail.com>
2020-12-18 09:38:56 +0100

    automatic calendar bar

--- src/task-management/js/eventhandler.js
+++ src/task-management/js/eventhandler.js
@@ -2,4 +2,29 @@
 
 window.onload = function () {
     console.log('Document loaded.')
+    loadCalendar();
+}
+
+
+$('#change-visibility').click(function() {
+    $('.visibility-popup').toggleClass("visibility-class");
+});
+
+
+function loadCalendar() {
+    let now = new Date();
+    now.setDate(now.getDate() - 5);
+   
+    $('.calendar-row').eq(0).append(` <div class="spacing-calendar">
+
+    </div>`);
+
+    for(let i = 1; i <= 18; i++) {
+        let newDate = new Date(now.setDate(now.getDate() + 1));
+        $('.calendar-row').append(`<div class="calendar-day"><p>${newDate.getDate()}</p></div>`);
+    }
+
+    $('.calendar-row').eq(0).append(` <div class="spacing-calendar">
+
+    </div>`);
 }
