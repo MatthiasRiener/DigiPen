04504635999e8a10126347de855b1ad4c4be73e5
Matthias Riener <matti.riener@gmail.com>
2020-12-27 15:26:17 +0100

    side-bar added structure - some bugs

--- src/components/side-bar/Sidebar.js
+++ src/components/side-bar/Sidebar.js
@@ -3,61 +3,185 @@
   const template = document.createElement('template');
 
   template.innerHTML = `
+
     <style>
 
-.header-nav {
-  height: 7vh;
-  background: #FFFFFF;
+.left-side-bar {
+    margin-top: 1%;
+    width: 17%;
+    height: 98%;
+    background: white;
+    box-shadow: 1px 0px 10px #00000020;
 
-  display: flex;
-  align-items: center;
-
-  box-shadow: 0px 1px 5px #00000050;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
-.header-nav p {
-  padding: 0 !important;
-  margin: 0 !important;
-
-  color: var(--dark-grey);
+.left-side-bar #title-nav {
+    font-size: 24px;
+    margin-top: 50px;
+    color: #38383880;
+    font-weight: bold;
 }
 
-.header-nav #logo {
-  font-weight: bold;
-  font-size: 26px;
-  margin: 0px 50px 0px 50px !important;
+.left-side-content {
+    width: 86%;
+    height: 100%;
 }
 
-.content-right-container {
-  margin-left: auto;
-  text-align: right;
+
+.workspace-container {
+    width: 100%;
+    height: 6vh;
+    border-radius: 15px;
+    border: 2px solid #70707020;
+
+    display: flex;
+    align-items: center;
 }
 
-.content-right-container p:nth-of-type(1) {
-  font-weight: bold;
+.cur-workspace {
+    height: 70%;
+    background: #5765FF;
+    margin: 3%;
+    border-radius: 15px;
 }
 
-.nav-profile-image {
-  width: 50px;
-  height: 50px;
-  background-position: center;
-  background-size: cover;
+#workspace-text {
+    margin-left: 20px !important;
+}
 
-  margin-right: 55px;
-  margin-left: 20px;
+.workspace-arrows {
+    margin-left: auto;
+    display: flex;
+    flex-direction: column;
+    margin-right: 20px;
+}
 
-  border-radius: 50%;
+
+.nav-bar-element {
+    width: 100%;
+    height: 5.5vh;
+    background: #E4E5E9;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+    color: #383838;
+    font-weight: bold;
+    font-size: 17px;
+}
+
+.nav-bar-element .icon-navbar {
+    margin-left: 10px;
+    font-size: 25px;
+}
+
+.nav-bar-element p {
+    margin-left: 20px;
+}
+
+.notification-navbar {
+    width: 30px;
+    height: 30px;
+    margin-left: auto;
+    margin-right: 10px;
+    background: var(--dark-grey);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 10px;
+    box-shadow: 0px 0px 5px var(--dark-grey);
+    color: white;
+}
+
+.notification-navbar p {
+    margin: 0 !important;
+}
+
+
+.nav-bar-element-subtask {
+    width: 80%;
+    height: 5.5vh;
+    background: #F8F7F9;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+    color: #383838;
+    font-weight: bold;
+    font-size: 17px;
+}
+
+.nav-bar-element-subtask .icon-navbar {
+    margin-left: 10px;
+    font-size: 25px;
+}
+
+.nav-bar-element-subtask p {
+    margin-left: 20px;
 }
     </style>
-    <div class="header-nav">
-    <p id="logo">Slidea</p>
-    <div class="content-right-container">
-      <p id="username" ></p>
-      <p id="role" ></p>
-    </div>
-    <div class="nav-profile-image"></div>
-  </div>
-  `;
+
+   <div class="left-side-bar">
+        <div class="left-side-content">
+          <p id="title-nav">Navigation</p>
+
+          <div class="workspace-container">
+            <div class="cur-workspace"></div>
+            <p id="workspace-text">4BHITM<br />Workspace</p>
+            <div class="workspace-arrows">
+              <ion-icon name="chevron-up-outline"></ion-icon>
+              <ion-icon name="chevron-down-outline"></ion-icon>
+            </div>
+          </div>
+
+          <div class="nav-bar-element">
+            <ion-icon name="albums-outline" class="icon-navbar"></ion-icon>
+            <p>Dashboard</p>
+          </div>
+
+          <div class="nav-bar-element">
+            <ion-icon name="checkbox-outline" class="icon-navbar"></ion-icon>
+            <p>Tasks</p>
+            <div class="notification-navbar"><p>3</p></div>
+          </div>
+
+          <div class="nav-bar-element">
+            <ion-icon name="people-outline" class="icon-navbar"></ion-icon>
+            <p>Workspaces</p>
+            <ion-icon
+              name="add-outline"
+              style="margin-left: auto; margin-right: 15px"
+              class="icon-navbar"
+            ></ion-icon>
+          </div>
+
+          <div class="nav-bar-element">
+            <ion-icon name="cog-outline" class="icon-navbar"></ion-icon>
+            <p>Settings</p>
+            <ion-icon
+              name="chevron-down-outline"
+              style="margin-left: auto; margin-right: 15px"
+              class="icon-navbar"
+            ></ion-icon>
+          </div>
+
+          <div class="nav-bar-element-subtask">
+            <ion-icon name="sunny-outline" class="icon-navbar"></ion-icon>
+            <p>Slidea</p>
+          </div>
+
+          <div class="nav-bar-element-subtask">
+            <ion-icon name="person-outline" class="icon-navbar"></ion-icon>
+            <p>Profile</p>
+          </div>
+        </div>
+      </div>
+`;
 
   class Sidebar extends HTMLElement {
     constructor() {
@@ -68,9 +192,6 @@
       });
       this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-      this.username = this.shadowRoot.querySelector('.content-right-container #username');
-      this.userrole = this.shadowRoot.querySelector('.content-right-container #role');
-      this.userImg = this.shadowRoot.querySelector('.nav-profile-image');
     }
 
     connectedCallback() {
@@ -83,5 +204,5 @@
 
     }
   }
-window.customElements.define('side-bar', SideBar);
+window.customElements.define('side-bar', Sidebar);
 })();
