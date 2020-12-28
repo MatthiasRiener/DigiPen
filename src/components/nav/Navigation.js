c6c20cb24b5c5bfd7afecc7d036d1581e72eae9e
Matthias Riener <matti.riener@gmail.com>
2020-12-28 14:00:55 +0100

    top-nb finished

--- src/components/nav/Navigation.js
+++ src/components/nav/Navigation.js
@@ -1,87 +1,44 @@
-(function () {
-  const template = document.createElement('template');
+import { template } from './template.js';
 
-  template.innerHTML = `
-    <style>
-     
-.header-nav {
-  height: 7vh;
-  background: #FFFFFF;
-
-  display: flex;
-  align-items: center;
-
-  box-shadow: 0px 1px 5px #00000050;
-}
-
-.header-nav p {
-  padding: 0 !important;
-  margin: 0 !important;
-
-  color: var(--dark-grey);
-}
-
-.header-nav #logo {
-  font-weight: bold;
-  font-size: 26px;
-  margin: 0px 50px 0px 50px !important;
-}
-
-.content-right-container {
-  margin-left: auto;
-  text-align: right;
-}
-
-.content-right-container p:nth-of-type(1) {
-  font-weight: bold;
-}
-
-.nav-profile-image {
-  width: 50px;
-  height: 50px;
-  background-position: center;
-  background-size: cover;
-
-  margin-right: 55px;
-  margin-left: 20px;
-
-  border-radius: 50%;
-}
-    </style>
-    <div class="header-nav">
-    <p id="logo">Slidea</p>
-    <div class="content-right-container">
-      <p id="username" ></p>
-      <p id="role" ></p>
-    </div>
-    <div class="nav-profile-image"></div>
-  </div>
-  `;
-
-  class Navigation extends HTMLElement {
+class TopNavigation extends HTMLElement {
     constructor() {
-      super();
+        super();
 
-      this.attachShadow({
-        mode: 'open'
-      });
-      this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.temp = document.createElement('template');
+        this.temp.innerHTML = template;
 
-      this.username = this.shadowRoot.querySelector('.content-right-container #username');
-      this.userrole = this.shadowRoot.querySelector('.content-right-container #role');
-      this.userImg = this.shadowRoot.querySelector('.nav-profile-image');
+        this.attachShadow({
+            mode: 'open',
+        });
+
+        this.shadowRoot.appendChild(this.temp.content.cloneNode(true));
+        this.loadCss(this.getAttribute("path"));
+
+        this.username = this.shadowRoot.querySelector('.content-right-container #username');
+        this.userrole = this.shadowRoot.querySelector('.content-right-container #role');
+        this.userImg = this.shadowRoot.querySelector('.nav-profile-image');
+    }
+
+    loadCss(path) {
+        fetch(path)
+        .then(response => response.text())
+            .then(data => {
+                let node = document.createElement('style');
+                node.innerHTML = data;
+                this.shadowRoot.appendChild(node);
+            });
     }
 
     connectedCallback() {
-      this.userImg.style.backgroundImage = `url('${this.getAttribute("img")}')`;
-      this.username.innerHTML = this.getAttribute('username');
-      this.userrole.innerHTML = this.getAttribute("role");
+        this.userImg.style.backgroundImage = `url('${this.getAttribute("img")}')`;
+        this.username.innerHTML = this.getAttribute('username');
+        this.userrole.innerHTML = this.getAttribute('role');
     }
 
     disconnectedCallback() {
 
     }
-  }
+}
 
-  window.customElements.define('nav-bar', Navigation);
-})();
+window.customElements.define('top-nb', TopNavigation);
+
