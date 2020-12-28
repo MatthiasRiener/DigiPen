f407080bc7fdcdf09d8173b9b2c17b78c04bb89f
LukasFriesenecker <friesilukas@gmail.com>
2020-12-28 13:22:31 +0100

    popups

--- src/editor/view/js/eventhandler.js
+++ src/editor/view/js/eventhandler.js
@@ -38,61 +38,87 @@ $('#content-leftSlides-slidesContent').scroll(function(){
 
 /* --------------- POPUPS --------------- */
 
+// open slides popup
 $('#content-leftSlides-topBar-down').click(function(){
     $('#popupBox').css('display', 'flex');
 });
 
+// close slides popup
 $('#popupBox-top-controls-buttons-exit').click(function(){
     $('#popupBox').css('display', 'none');
 });
 
+// open add-object popup
 $('#content-navigation-first-right-icon').click(function(){
     $('#addObjectPopup').css('display', 'flex');
     $('#addObjectPopup-inner-popup').css('display', 'flex');
+    $('#addObjectPopup-inner-popup').animate({
+        margin: "1.5vh",
+        opacity: 1.0
+    }, 100);
 });
 
+// open image popup in add-object popup
+$('#addObjectPopup-inner-popup-image').click(function(){
+    $('#imagePopup').css('display', 'flex');
+    $('#imagePopup-inner-unsplash').css('display', 'flex');
+    $('#imagePopup-inner-unsplash').animate({
+        margin: "1.5vh 0.8vh",
+        opacity: 1.0
+    }, 100);
+});
+
+// close all add-objects popups
 $(document).click(function(event) {
-    if (!$(event.target).closest("#addObjectPopup-inner-popup, #addObjectPopup-inner-unsplash, #content-navigation-first-right-icon").length) {
+    if (!$(event.target).closest("#addObjectPopup-inner-popup, #content-navigation-first-right-icon, #imagePopup-inner-unsplash, #addObjectPopup-inner-popup-image").length) {
         $('#addObjectPopup').css('display', 'none');
         $('#addObjectPopup-inner-popup').css('display', 'none');
-        $('#addObjectPopup-inner-unsplash').css('display', 'none');
+        $('#addObjectPopup-inner-popup').css('margin', '0vh 1.5vh');
+        $('#addObjectPopup-inner-popup').css('opacity', '0.0');
+
+        $('#imagePopup').css('display', 'none');
+        $('#imagePopup-inner-unsplash').css('display', 'none');
+        $('#imagePopup-inner-unsplash').css('margin', '1.5vh 0.8vh');
+        $('#imagePopup-inner-unsplash').css('opacity', '0.0');
     }
 });
 
-$('#addObjectPopup-inner-popup-image').click(function(){
-    $('#imagePopup').css('display', 'flex');
-});
-
+// open settings popup
 $('#content-navigation-second-settings').click(function() {
     $('#settingsPopup').css('display', 'flex');
 });
 
+// close settings popup
+$('#settingsPopup-inner-bottom-header-exit').click(function() {
+    $('#settingsPopup').css('display', 'none');
+    $('#settingsPopup-inner-bottom-button-join').css('display', 'none');
+});
+
+// open cam popup in settings popup
 $('#settingsPopup-inner-bottom-settings-cam-bottom-popup').click(function() {
     $('#camPopup').css('display', 'flex');
 });
 
+// close cam popup in settings popup
 $(document).click(function(event) {
     if (!$(event.target).closest("#camPopup-inner-popup, #settingsPopup-inner-bottom-settings-cam-bottom-popup").length) {
         $('#camPopup').css('display', 'none');    
     }
 });
 
+// open mic popup in settings popup
 $('#settingsPopup-inner-bottom-settings-mic-bottom-popup').click(function() {
     $('#micPopup').css('display', 'flex');
 });
 
+// close mic popup in settings popup
 $(document).click(function(event) {
     if (!$(event.target).closest("#micPopup-inner-popup, #settingsPopup-inner-bottom-settings-mic-bottom-popup").length) {
         $('#micPopup').css('display', 'none');    
     }
 });
 
-
-$('#settingsPopup-inner-bottom-header-exit').click(function() {
-    $('#settingsPopup').css('display', 'none');
-    $('#settingsPopup-inner-bottom-button-join').css('display', 'none');
-});
-
+// open presentation-mode popup
 $('#content-navigation-fifth-box-down').click(function(){
     $('#presentationModePopup').css('display', 'flex');
     $('#presentationModePopup-inner-popup').css('display', 'flex');
@@ -102,16 +128,12 @@ $('#content-navigation-fifth-box-down').click(function(){
     }, 100);
 });
 
-
+// close presentation-mode popup
 $(document).click(function(event) {
     if (!$(event.target).closest("#presentationModePopup-inner-popup, #content-navigation-fifth-box-down").length) {
-        $('#presentationModePopup-inner-popup').animate({
-            margin: "0vh 1.5vh",
-            opacity: 0.0
-        }, 10);
-        setTimeout(function() {
-            $('#presentationModePopup-inner-popup').css('display', 'none');
-            $('#presentationModePopup').css('display', 'none');
-        }, 100);
+        $('#presentationModePopup-inner-popup').css('display', 'none');
+        $('#presentationModePopup').css('display', 'none');
+        $('#presentationModePopup-inner-popup').css('margin', '1vh 1.5vh');
+        $('#presentationModePopup-inner-popup').css('opacity', '0.0');
     }
 });
