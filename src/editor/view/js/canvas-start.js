ef331dd0f91101dacd65f7427d2a2fd878a14fe5
Matthias Riener <matti.riener@gmail.com>
2021-01-02 15:55:38 +0100

    custom ng if

--- src/editor/view/js/canvas-start.js
+++ src/editor/view/js/canvas-start.js
@@ -23,7 +23,8 @@ var props = {
 };
 
 
-var figureEditor = false, textEditor = false;
+var figureEditor = false,
+    textEditor = false;
 
 
 $(document).ready(function () {
@@ -32,8 +33,10 @@ $(document).ready(function () {
 
 });
 
+var bruh = true;
 
 $('#addObjectPopup-inner-popup-text').on('click', function () {
+   bruh = !bruh;
     addText();
 });
 
@@ -60,15 +63,27 @@ function init() {
             }
 
 
-            switch(selectedObject.type) {
+            switch (selectedObject.type) {
                 case 'rect':
                 case 'circle':
                 case 'triangle':
-                  figureEditor = true;
-                  getFill();
-                  break;
+                    figureEditor = true;
+                    getFill();
+                    break;
                 case 'i-text':
                     textEditor = true;
+                    getLineHeight();
+                    getCharSpacing();
+                    getBold();
+                    getFill();
+                    getTextDecoration();
+                    getTextAlign();
+                    getFontFamily();
+
+                    console.log(getFontFamily()); 
+                    break;
+
+                case 'image':
                     break;
             }
         },
@@ -98,7 +113,6 @@ function addText() {
     canvas.add(text);
     selectItemAfterAdded(text);
 
-    console.log("added element");
 }
 
 
@@ -140,10 +154,12 @@ function getId() {
 function getActiveStyle(styleName, object) {
     object = object || canvas.getActiveObject();
 
-    if(!object) { return ''; }
+    if (!object) {
+        return '';
+    }
 
-    if(object.getSelectionStyles && object.isEditing) {
-        return (object.getSelectionStyles()[styleName] || '');
+    if (object.getSelectionStyles && object.isEditing) {
+        return (object.getSelectionStyles()[styleName] || '');
     } else {
         return (object[styleName] || '');
     }
@@ -159,5 +175,93 @@ function getOpacity() {
 
 
 function getFill() {
-    this.props.fill = this.getActiveStyle('fill', null);
+    props.fill = getActiveStyle('fill', null);
 }
+
+function getLineHeight() {
+    props.lineHeight = getActiveStyle('lineHeight', null);
+}
+
+function getCharSpacing() {
+    props.charSpacing = getActiveStyle('charSpacing', null);
+}
+
+function getFontSize() {
+    props.fontSize = getActiveStyle('fontSize', null);
+}
+
+function getBold() {
+    props.fontWeight = getActiveStyle('fontWeight', null);
+}
+
+
+function getTextDecoration() {
+    props.TextDecoration = getActiveStyle('textDecoration', null);
+}
+
+function getTextAlign() {
+    props.textAlign = getActiveProp('textAlign');
+}
+
+function getFontFamily() {
+    props.fontFamily = getActiveProp('fontFamily');
+}
+
+function getActiveProp(name) {
+    const object = canvas.getActiveObject();
+    if(!object) {return ''; }
+    return object[name] || '';
+}
+
+
+/*------------------------Custom NGIF------------------------*/
+
+
+
+var CustomNGIf = function(element, callback, propertyName) {
+    var _value = null;
+  
+    // Create copies of elements do that you can store/use it in future 
+    this.parent = element.parentNode;
+    this.element = element;
+    this.clone = null;
+  
+    // Create a property that is supposed to be watched
+    Object.defineProperty(this, propertyName, {
+      get: function() {
+        return _value;
+      },
+      set: function(value) {
+        // If same value is passed, do nothing.
+        if (_value === value) return;
+        _value = !!value;
+        this.handleChange(_value);
+      }
+    });
+  
+    this.handleChange = function(value) {
+      this.clone = this.element.cloneNode(true);
+      if (_value) {
+        var index = Array.from(this.parent.children).indexOf(this.element);
+  
+        // Check if element is already existing or not.
+        // This can happen if some code breaks before deleting node.
+        if (index >= 0) return;
+        this.element = this.clone.cloneNode(true);
+        this.parent.appendChild(this.element);
+      } else {
+        this.element.remove();
+      }
+  
+      // For any special handling
+      callback && callback();
+    }
+  }
+
+
+
+var div = document.getElementById('test-ngif');
+var obj = new CustomNGIf(div, function() {
+    
+}, bruh);
+
