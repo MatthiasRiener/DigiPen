db75fcac4f0209f01b8aea4db703bd144e581c87
Matthias Riener <matti.riener@gmail.com>
2021-01-02 15:26:05 +0100

    working on editor

--- src/editor/view/js/canvas-start.js
+++ src/editor/view/js/canvas-start.js
@@ -4,6 +4,27 @@ var canvas = this.__canvas = new fabric.Canvas('canvas', {
     selectionBorderColor: 'blue',
 });
 
+var selected;
+
+var props = {
+    canvasFill: '#ffffff',
+    canvasImage: '',
+    id: null,
+    opacity: null,
+    fill: null,
+    fontSize: null,
+    lineHeight: null,
+    charSpacing: null,
+    fontWeight: null,
+    fontStyle: null,
+    textAlign: null,
+    fontFamily: null,
+    TextDecoration: ''
+};
+
+
+var figureEditor = false, textEditor = false;
+
 
 $(document).ready(function () {
 
@@ -22,9 +43,43 @@ function init() {
     canvas.setHeight($('#content-main-inner-spacing-middle').height());
 
     canvas.on({
-        
+        'object:moving': (e) => {},
+        'object:modified': (e) => {},
+        'selection:created': (e) => {
+            const selectedObject = e.target;
+            selected = selectedObject;
+            selectedObject.hasRotationPoint = true;
+            selectedObject.transparentCorners = false;
+            selectedObject.cornerColor = 'rgba(255, 87, 34, 0.7)';
+
+            resetPanels();
+
+            if (selectedObject.type !== 'group' && selectedObject) {
+                getId();
+                getOpacity();
+            }
+
+
+            switch(selectedObject.type) {
+                case 'rect':
+                case 'circle':
+                case 'triangle':
+                  figureEditor = true;
+                  getFill();
+                  break;
+                case 'i-text':
+                    textEditor = true;
+                    break;
+            }
+        },
+        'selection:cleared': (e) => {
+            selected = null;
+            resetPanels();
+        }
     })
 }
+
+
 
 function addText() {
     const text = new fabric.IText('hallo', {
@@ -68,4 +123,41 @@ function selectItemAfterAdded(obj) {
 
 function randomId() {
     return Math.floor(Math.random() * 999999) + 1;
+}
+
+
+function resetPanels() {
+
+}
+
+
+function getId() {
+    props.id = canvas.getActiveObject().toObject().id;
+}
+
+
+
+function getActiveStyle(styleName, object) {
+    object = object || canvas.getActiveObject();
+
+    if(!object) { return ''; }
+
+    if(object.getSelectionStyles && object.isEditing) {
+        return (object.getSelectionStyles()[styleName] || '');
+    } else {
+        return (object[styleName] || '');
+    }
+}
+
+
+/*------------------------Styles Functions------------------------*/
+
+
+function getOpacity() {
+    props.opacity = getActiveStyle('opacity', null) * 100;
+}
+
+
+function getFill() {
+    this.props.fill = this.getActiveStyle('fill', null);
 }
