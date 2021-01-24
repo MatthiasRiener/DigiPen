06150f2f7e60f444384604072bdf32884bca145f
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-01-24 14:47:33 +0100

    cookie container

---
+++ src/client/components/cookie-container/CookieContainer.js
@@ -0,0 +1,42 @@
+import { template } from './template.js';
+
+class CookieContainer extends HTMLElement {
+    constructor() {
+        super();
+
+        this.temp = document.createElement('template');
+        this.temp.innerHTML = template;
+
+        this.attachShadow({
+            mode: 'open',
+        });
+
+        this.shadowRoot.appendChild(this.temp.content.cloneNode(true));
+        this.loadCss(this.getAttribute("path"));
+
+        this.agree = this.shadowRoot.querySelector('#agree');
+        this.disagree = this.shadowRoot.querySelector('#disagree');
+
+    }
+
+    loadCss(path) {
+        fetch(path)
+            .then(response => response.text())
+            .then(data => {
+                let node = document.createElement('style');
+                node.innerHTML = data;
+                this.shadowRoot.appendChild(node);
+            });
+    }
+
+    connectedCallback() {
+        this.agree.addEventListener('click', (e) => {
+            console.warn('Cookies saved!');
+        });
+    }
+
+    disconnectedCallback() { }
+}
+
+window.customElements.define('cookie-container', CookieContainer);
+
---
+++ src/client/components/cookie-container/template.js
@@ -0,0 +1,16 @@
+export const template = `<div class="cookie-placer">
+    <div class="cookie-container-box">
+        <i class="fas fa-cookie-bite"></i>
+        <div class="cookie-container-content">
+            We use cookies and other tracking technologies to improve your browsing experience on our website, to show
+            you
+            personalized content and targeted ads, to analyze our website traffic, and to understand where our visitors
+            are
+            coming from.<br>
+            <a href="u fu*ging rl 🤘">Learn more</a>
+        </div>
+        <button id="agree">Okay</button>
+    </div>
+</div>
+<script src="https://kit.fontawesome.com/a076d05399.js" defer></script>`;
+
