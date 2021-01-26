653ff68a76487d989b22d26a56b41cf5e214dee3
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-01-26 12:45:46 +0100

    create user tested

--- src/server/tests/test_auth.py
+++ src/server/tests/test_auth.py
@@ -1,8 +1,28 @@
-from ..app.endpoints.auth.repository.AuthenticationRepository import AuthenticationRepository
+from ..app.reprository.AuthenticationRepository import AuthenticationRepository
 import pytest
-
+import time
 
 auth = AuthenticationRepository()
+
+@pytest.mark.parametrize('user_id, name, img, last_login, result', [
+    (1, "Max", None, time.time(), "User 1 was successfully inserted"),
+    (2, "Max123", None, None, "The username can only contain alphabetical letters"),
+    (3, "Max", None, "invalid", "The time format is invalid"),
+    (4, "Max", None, None, "User 4 was successfully inserted."),
+    (1, "Max", None, None, "There is already a user with the userid 1"),
+    (5, None, None, None, "No information was given regarding the users username"),
+    (None, None, None, None, "No information was given regarding the users userid")
+])
+def test_createUser(user_id, name, img, last_login, result):
+    assert auth.createUser(user_id, name, img, last_login) == result
+
+@pytest.mark.parametrize('user_id, result', [
+    (None, "No information was given regarding the users userid"),
+    (1, )
+])
+def test_retrieveUser(user_id, result):
+    assert auth.retrieveUser(user_id) == result
+
 
 @pytest.mark.parametrize('x, y, result', [
     (10, 10, 20),
