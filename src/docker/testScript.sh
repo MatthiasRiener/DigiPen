763831a399c75cd6a0e573f0985d30a4abd5edbc
Matthias Riener <matti.riener@gmail.com>
2021-01-31 16:05:14 +0100

    working now?

---
+++ src/docker/testScript.sh
@@ -0,0 +1,8 @@
+
+echo $PWD
+docker-compose up -d
+docker stop keycloak
+docker rm keycloak
+docker run -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e KEYCLOAK_IMPORT="/tmp/realm-export.json -Dkeycloak.profile.feature.upload_scripts=enabled" -v "$(pwd)/imports/realm/realm-export.json:/tmp/realm-export.json" --name keycloak jboss/keycloak
+echo "script beendet..."
+docker stop $(docker ps -a -q)
