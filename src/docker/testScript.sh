6d3934fc36e80839a49d755a49dcbbacd5729129
Matthias Riener <matti.riener@gmail.com>
2021-01-31 17:32:23 +0100

    mama mia

--- src/docker/testScript.sh
+++ src/docker/testScript.sh
@@ -1,16 +1,21 @@
 
-echo $PWD
-docker-compose up -d
 docker stop keycloak
+docker rm keycloak
 
-containerName="keycloak"
-if docker ps -a --format '{{.Names}}' | grep -Eq "^${containerName}\$"; then
-    docker start keycloak
-    echo "restarting keycloak..."
-else
-  docker run -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e KEYCLOAK_IMPORT="/tmp/realm-export.json -Dkeycloak.profile.feature.upload_scripts=enabled" -v "$(pwd)/imports/realm/realm-export.json:/tmp/realm-export.json" --name keycloak jboss/keycloak
-fi
+docker run -d --name keycloak jboss/keycloak
+
+CONTAINER_ID=$(docker ps -aqf "name=keycloak" | head -1)
+echo $CONTAINER_ID
+
+echo "WAITING FOR KEYCLOAK TO START UP :D"
+sleep 20
+
+docker commit $CONTAINER_ID friesimatts/keycloak
+
+echo "IMAGE WAS CREATED SUCESFULLY :D"
+
+echo "STARTING COMPOSERU"
+
+docker-compose up
 
 
-echo "script beendet..."
-docker stop $(docker ps -a -q)
