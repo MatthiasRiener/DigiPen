6eebd6effe74f81a978f54872e004291abbf9210
Matthias Riener <matti.riener@gmail.com>
2021-02-02 15:48:54 +0100

    dads

--- src/server/app/repository/WorkspaceRepository.py
+++ src/server/app/repository/WorkspaceRepository.py
@@ -10,10 +10,10 @@ class WorkspaceRepository():
     def __init__(self, testing):
         self.testing = testing
 
-    def createWorkspace(self, name, users, img, user_id):
+    def createWorkspace(self, name, users, img, creator):
 
         Workspace(w_id=str(time.time()), w_name=name, w_img=img, w_users=uRepo.getUserIds(users=users)).save()
-        return json.dumps({"workspaces": self.getRepoCounter(u_id=user_id)})
+        return json.dumps({"workspaces": self.getRepoCounter(u_id=creator)})
 
     def getRepoCounter(self, u_id):
         return Workspace.objects(w_users__in=[u_id]).count()
