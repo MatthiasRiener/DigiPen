db8b474bbe088c6230cfd89ea4b155c8b6a90cc1
LukasFriesenecker <friesilukas@gmail.com>
2021-02-03 14:09:41 +0100

    user since

--- src/server/app/files/static/profile/js/requests.js
+++ src/server/app/files/static/profile/js/requests.js
@@ -7,4 +7,8 @@ sendRequestToServer({type: "GET", url: "/profile/user"}).then(data => {
 
     // Insert workspace count
     $('#workspaceCount').text(data.workspaces);
+
+    // Insert created date
+    let date = new Date(data.created * 1000).toLocaleDateString("en-US", {month: "long", day: "2-digit", year: "numeric"});
+    $('#userSince').text(date);
 });
