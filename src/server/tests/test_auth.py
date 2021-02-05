6ffc2eea928c124695685dadc3276b04401f841b
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-02-05 08:51:23 +0100

    created tested

--- src/server/tests/test_auth.py
+++ src/server/tests/test_auth.py
@@ -3,10 +3,11 @@ from ..app.repository.AuthenticationRepository import AuthenticationRepository
 import json
 import re
 import uuid 
+import time
 
 from ..app.models.User import User
 from mongoengine import disconnect, connect
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
    
 auth = AuthenticationRepository(testing=True) 
 
@@ -25,21 +26,26 @@ def app():
     # clear db to run tests
     
 keycloakid = str(uuid.uuid4())
-lastlogin = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+# lastlogin = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+lastlogin = time.time()
 
 # last login, created nochmal überprüfen (datetime.today() -> time.time())
 
-@pytest.mark.parametrize('user_id, name, img, last_login, result', [
-    (keycloakid, "Max", None, lastlogin, "User %s was successfully inserted." % (keycloakid)),
-    (keycloakid, "Max", None, None, "Last login must not be None"),
-    (str(uuid.uuid4()), "Max123", None, lastlogin, "The username can only contain alphabetical letters"),
-    (str(uuid.uuid4()), "Max", None, datetime.today().strftime("%Y/%m/%d"), "The time format is invalid. It has to be the following: %s (Bsp.: %s)" % ('%Y-%m-%d %H:%M:%S', lastlogin)),
-    (keycloakid, "Max", None, lastlogin, "There is already a user with the userid %s" % (keycloakid)),
-    (str(uuid.uuid4()), None, None, lastlogin, "No information was given regarding the users username"),
-    (str(uuid.uuid4()), "Max", None, (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S"), "Last login must not be in the future"),
-    (None, None, None, None, "No information was given")
+@pytest.mark.parametrize('user_id, name, img, last_login, created, result', [
+    (keycloakid, "Max", None, lastlogin, lastlogin, "User %s was successfully inserted." % (keycloakid)),
+    (keycloakid, "Max", None, None, lastlogin, "Last login must not be None"),
+    (keycloakid, "Max", None, lastlogin, None, "The time when the user was created must not be None"),
+    (str(uuid.uuid4()), "Max123", None, lastlogin, lastlogin, "The username can only contain alphabetical letters"),
+    (str(uuid.uuid4()), "Max", None, str(time.time()), lastlogin, "The time format of last login is invalid. It has to a integer (Bsp.: %s)" % (lastlogin)),
+    (str(uuid.uuid4()), "Max", None, time.time(), str(lastlogin), "The time format of created is invalid. It has to a integer (Bsp.: %s)" % (lastlogin)),
+    (keycloakid, "Max", None, lastlogin, lastlogin, "There is already a user with the userid %s" % (keycloakid)),
+    (str(uuid.uuid4()), None, None, lastlogin, lastlogin, "No information was given regarding the users username"),
+    (str(uuid.uuid4()), "Max", None, lastlogin * 2, lastlogin, "Last login must not be in the future"),
+    (str(uuid.uuid4()), "Max", None, lastlogin, lastlogin * 2, "Created must not be in the future"),
+    (str(uuid.uuid4()), "Max", None, lastlogin / 2, lastlogin, "Last login be equal/greater then created"),
+    (None, None, None, None, None, "No information was given")
 ])
-def test_createUser(user_id, name, img, last_login, result):
+def test_createUser(user_id, name, img, last_login, created, result):
     assert auth.createUser(user_id, name, img, last_login) == result
 
 dummyid = str(uuid.uuid4())
