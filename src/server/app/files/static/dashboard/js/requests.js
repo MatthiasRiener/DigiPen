e7925d712505301cf8a65bfd79a574885bfa78b2
LukasFriesenecker <friesilukas@gmail.com>
2021-02-08 12:24:42 +0100

    create presentation

--- src/server/app/files/static/dashboard/js/requests.js
+++ src/server/app/files/static/dashboard/js/requests.js
@@ -1,8 +1,19 @@
+let keywords = [];
+
+$('#addkeyword').keypress(function (e) {
+    if (e.which == 13 && !keywords.includes($('#addkeyword').val())) {
+        keywords.push($('#addkeyword').val());
+        $('.keywords').append('<span>' + $('#addkeyword').val() + '</span>');
+        $('#addkeyword').val('');
+    }
+  });
+
 $('#submitcontrolls').click(function () {
-    console.log($('#template_title').text());
+    console.log(keywords);
 
     let presentation = {
-        name: $('#template_title').text()
+        name: $('#template_title').text(),
+        keywords: keywords
     }
 
     sendRequestToServer({type: "POST", url: "/dashboard/createPresentation", data: presentation}).then(data => {
