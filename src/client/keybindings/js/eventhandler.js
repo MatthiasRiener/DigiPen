3d63782d834ba8028eaa92266f3d27e0627ffb5e
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-02-10 20:37:00 +0100

    color management

--- src/client/keybindings/js/eventhandler.js
+++ src/client/keybindings/js/eventhandler.js
@@ -3,14 +3,19 @@
 let optionscount = 0,
     oldval,
     jsondata;
-var infoOuter;
-var info, oldInfo = null;
+let infoOuter;
+let info, oldInfo = null;
 
-var lastEvent;
-var heldKeys = [];
+let lastEvent;
+let heldKeys = [];
 let jsondataelem;
 let keysPushed = [];
 let dublicate = false;
+
+let highlightColor = 'rgba(100, 198, 237, 1)';
+let outoffocusColor = 'rgba(100, 198, 237, 0.7)';
+let emptyslotColor = 'rgba(100, 198, 237, 0.4)';
+let dublicateColor = 'rgba(255,0,0,0.7)';
 
 $.getJSON("ajax/shortcuts.json", function (data) {
     jsondata = data;
@@ -20,7 +25,7 @@ $.getJSON("ajax/shortcuts.json", function (data) {
         if (element.keys.length > 0) {
             $("#bindings .keybindinginput").eq(optionscount).text(element.keys.join(' + ').replace('Key', '').replace(/([A-Z])/g, ' $1').trim());
         } else
-            $("#bindings .keybindinginput").eq(optionscount).css('background-color', 'rgba(100, 198, 237, 0.4)');
+            $("#bindings .keybindinginput").eq(optionscount).css('background-color', emptyslotColor);
 
         $("#bindings .keybindinginput").eq(optionscount).on('focusin', function (event) {
             oldval = this.value;
@@ -38,9 +43,9 @@ $.getJSON("ajax/shortcuts.json", function (data) {
         $("#bindings .keybindinginput").eq(optionscount).click(function (event) {
             infoOuter = this;
             if (this.dataset.dublicate != "true")
-                this.style.backgroundColor = 'rgba(100, 198, 237, 1)';
+                this.style.backgroundColor = highlightColor;
             if (oldInfo != this && oldInfo != null && oldInfo.dataset.dublicate != "true") {
-                oldInfo.style.backgroundColor = 'rgba(100, 198, 237, .7)';
+                oldInfo.style.backgroundColor = outoffocusColor;
             }
             oldInfo = this;
             jsondataelem = element;
@@ -69,7 +74,7 @@ window.onkeydown = async function (event) {
 
     [...$(".keybindinginput")].forEach(keybindingelem => {
         if (keybindingelem.innerText === [...new Set(findDuplicates(keysPushed))].toString()) {
-            keybindingelem.style.backgroundColor = "red"
+            keybindingelem.style.backgroundColor = dublicateColor;
             keybindingelem.dataset.dublicate = "true";
         } else {
             keybindingelem.style.backgroundColor = "rgba(100, 198, 237, .7)"
@@ -81,7 +86,7 @@ window.onkeydown = async function (event) {
         dublicate = true;
     } else {
         dublicate = false;
-        infoOuter.style.backgroundColor = 'rgba(100, 198, 237, 1)';
+        infoOuter.style.backgroundColor = highlightColor;
     }
 
     keysPushed = [];
