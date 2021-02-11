a5a8c2d808936983b83f09df0e2e0b36590c975f
LukasFriesenecker <friesilukas@gmail.com>
2021-02-12 00:34:23 +0100

    noti popup

--- src/server/app/files/static/components/check-notifications/template.js
+++ src/server/app/files/static/components/check-notifications/template.js
@@ -2,6 +2,49 @@
   export const template =
     `
     <div class="notifications-container">
+      <div class="top">
+        <h1>Notifications</h1>
+      </div>
+      <div class="bottom">
+        <div class="noti-row">
+          <h2 class="noti-row-text">2 new invites</h2>
+          <ion-icon id="noti-row-arrow" name="chevron-forward-outline"></ion-icon>
+        </div>
+      </div>
+    </div>
 
+    <div class="invites-container">
+      <div class="top">
+        <h1>Invites</h1>
+      </div>
+      <div class="bottom">
+        <div class="invites-row">
+          <div class="invites-row-left">
+            <div class="invites-picture"></div>
+          </div>
+          <div class="invites-row-middle">
+            <h2 class="invites-presentation">Presentation</h2>
+            <h2 class="invites-owner">Lukas Friesenecker</h2>
+          </div>
+          <div class="invites-row-right">
+            <button class="invites-accept"><ion-icon name="close-outline"></ion-icon></button>
+            <button class="invites-decline"><ion-icon name="checkmark-outline"></ion-icon></button>
+          </div>
+        </div>
+
+        <div class="invites-row">
+          <div class="invites-row-left">
+            <div class="invites-picture"></div>
+          </div>
+          <div class="invites-row-middle">
+            <h2 class="invites-presentation">Presentation</h2>
+            <h2 class="invites-owner">Lukas Friesenecker</h2>
+          </div>
+          <div class="invites-row-right">
+            <button class="invites-accept"><ion-icon name="close-outline"></ion-icon></button>
+            <button class="invites-decline"><ion-icon name="checkmark-outline"></ion-icon></button>
+          </div>
+        </div>
+      </div>
     </div>
     `;
