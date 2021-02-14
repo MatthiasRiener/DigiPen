e9b81808d920447bc508ffa9a7a2c80329ed38b4
Matthias Riener <matti.riener@gmail.com>
2021-02-14 13:17:49 +0100

    get presentations working

--- src/server/app/endpoints/task_management/controllers.py
+++ src/server/app/endpoints/task_management/controllers.py
@@ -1,11 +1,22 @@
 from flask import Flask, Blueprint, render_template, abort, g, request
 
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt, set_access_cookies, get_jti,
+                                set_refresh_cookies, unset_jwt_cookies, decode_token)
+
+
+from ...repository.repository import TaskRepositoryInstance
 
 task_m = Blueprint("task_management", __name__,
                     static_folder="static", template_folder="templates")
 
-
+taskRepo = TaskRepositoryInstance()
 
 @task_m.route('/', methods=["GET"])
 def index():
     return render_template('/task_management/index.html')
+
+@task_m.route('/getTasks', methods=["GET"])
+@jwt_required
+def getTasksRoute():
+    user_id = get_jwt_identity()
+    return taskRepo.repo.getPresentationsForTask(u_id=user_id)
--- src/server/app/repository/TaskRepository.py
+++ src/server/app/repository/TaskRepository.py
@@ -3,7 +3,12 @@ from ..models.Task import Task
 import uuid
 from ..db.settings import mongoclient
 
-from .repository import PresentationRepositoryInstace
+import json
+
+from .PresentationRepository import PresentationRepository
+
+presRepo = PresentationRepository(testing=True)
+
 
 class TaskRepository():
     def __init__(self, testing):
@@ -11,11 +16,27 @@ class TaskRepository():
 
     def createTaskList(self, p_id):
         TaskList(p_id=p_id, t_id=str(uuid.uuid4())).save()
-       
+
         print("task created!!! :D")
 
+    def getPresentationsForTask(self, u_id):
+        pres = presRepo.getUsersPresentation(user_id=u_id)
+        
+        for p in pres:
+            print(p["_id"])
+            color = self.getColor(p["_id"])
+            print(color)
+            p["color"] = color
+        return json.dumps({"res": pres})
+
+    def getColor(self, p_id):
+        taskList = TaskList.objects(p_id=p_id).first()
+
+        if taskList is None:
+            return '#555555'
+        return taskList.t_color
     def deleteAllTasks(self):
-        if testing:
+        if self.testing:
             Task.objects().delete()
             TaskList.objects().delete()
             return 'All tasks dropped!'
