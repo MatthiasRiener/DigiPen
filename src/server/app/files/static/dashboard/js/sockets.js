cd5c0baa518cfe4125b17cb5c8111d3737300b81
LukasFriesenecker <friesilukas@gmail.com>
2021-02-14 11:57:58 +0100

    invites

--- src/server/app/files/static/dashboard/js/sockets.js
+++ src/server/app/files/static/dashboard/js/sockets.js
@@ -1,26 +1,3 @@
-var socket = io();
-        
-socket.on('connect', function() {
-    sendRequestToServer({type: "GET", url: "/auth/getUserID"}).then(data => {
-        console.log(data)
-        socket.emit('connectUser', {user_id: data.u_id});
-    });
-});
-
-socket.on('message', function(data) {
-    console.log('on message: ' + data);
-});
-
-socket.on('error', function(data) {
-    console.log('on error: ' + data);
-    socket.disconnect();
-});
-
-socket.on('disconnect', function(data) {
-    console.log('on disconnect: ' + data);
-    socket.connect();
-});
-
 socket.on('searchUser', function (data) {
     if(data === undefined) {
         return;
