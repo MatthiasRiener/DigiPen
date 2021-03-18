9f93fffa67d3a4cec59e475760aceba3ad79d0cb
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-03-18 20:10:50 +0100

    format is back

--- src/server/app/repository/CanvasRepository.py
+++ src/server/app/repository/CanvasRepository.py
@@ -17,40 +17,8 @@ class CanvasRepository():
 
         print(canvasObj[0])"""
 
-        mongoclient.db['canvas'].insert_one({
-            "p_id": p_id,
-            "canvas": {
-                "objects": [
-                    {
-                        "type": "rect",
-                        "left": 50,
-                        "top": 50,
-                        "width": 20,
-                        "height": 20,
-                        "fill": "green",
-                        "overlayFill": None,
-                        "stroke": None,
-                        "strokeWidth": 1,
-                        "strokeDashArray": None,
-                        "scaleX": 1,
-                        "scaleY": 1,
-                        "angle": 0,
-                        "flipX": False,
-                        "flipY": False,
-                        "opacity": 1,
-                        "selectable": True,
-                        "hasControls": True,
-                        "hasBorders": True,
-                        "hasRotatingPoint": False,
-                        "transparentCorners": True,
-                        "perPixelTargetFind": False,
-                        "rx": 0,
-                        "ry": 0
-                    }
-                ],
-                "background": "rgba(0, 0, 0, 0)"
-            }
-        })
+        mongoclient.db['canvas'].insert_one({"p_id": p_id, "canvas": {"objects": [{"type": "rect", "left": 50, "top": 50, "width": 20, "height": 20, "fill": "green", "overlayFill": None, "stroke": None, "strokeWidth": 1, "strokeDashArray": None, "scaleX": 1, "scaleY": 1,
+                                                                                   "angle": 0, "flipX": False, "flipY": False, "opacity": 1, "selectable": True, "hasControls": True, "hasBorders": True, "hasRotatingPoint": False, "transparentCorners": True, "perPixelTargetFind": False, "rx": 0, "ry": 0}], "background": "rgba(0, 0, 0, 0)"}})
         print("Canvas created... :D")
 
     def getCanvas(self, p_id):
