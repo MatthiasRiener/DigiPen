bf83620409670d7499bd712dca9feb3c2c80d879
Matthias Riener <matti.riener@gmail.com>
2021-03-24 11:39:35 +0100

    massive co... commit ;)

--- src/server/app/repository/CanvasRepository.py
+++ src/server/app/repository/CanvasRepository.py
@@ -29,10 +29,10 @@ class CanvasRepository():
 
         res = mongoclient.db['canvas'].insert_one({"p_id": p_id, "s_id": s_id, "canvas": {"objects": [{"type": "rect", "left": 50, "top": 50, "width": 20, "height": 20, "fill": "green", "overlayFill": None, "stroke": None, "strokeWidth": 1, "strokeDashArray": None, "scaleX": 1, "scaleY": 1,
                                                                                    "angle": 0, "flipX": False, "flipY": False, "opacity": 1, "selectable": True, "hasControls": True, "hasBorders": True, "hasRotatingPoint": False, "transparentCorners": True, "perPixelTargetFind": False, "rx": 0, "ry": 0}], "background": "rgba(0, 0, 0, 0)"}})
-        return res
+        return self.getSpecificSlide(object_id=res.inserted_id)
 
-    def getSpecificSlide(self, p_id, s_id):
-        return mongoclient.db["canvas"].find_one({"p_id", p_id, "s_id", s_id})
+    def getSpecificSlide(self, object_id):
+        return mongoclient.db["canvas"].find_one({"_id": ObjectId(object_id)})
 
     def getCanvas(self, p_id):
         return mongoclient.db['canvas'].find({"p_id": p_id})
