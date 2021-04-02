7d9ace1a701789dfe24c726965cc7d97a607ee49
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-04-02 18:46:12 +0200

    setup feedback-form environment

---
+++ src/server/app/files/static/components/feedback-form/template.js
@@ -0,0 +1,17 @@
+
+  export const template =
+    `
+    <div class="workspace-container">
+        <div class="left-side-container">
+            <p id="workspace-title">Create Workspace</p>
+            <p class="textfield-title">Name</p>
+            <input id="workspaceName" type="text" placeholder="Name" />
+            <p class="textfield-title">Invite Users</p>
+            <textarea id="workspaceUsers" class="inv-user-txt" placeholder="Name"></textarea>
+            <div class="btn-create-ws"><p>Create</p></div>
+        </div>
+        <div class="right-side-container">
+            <div class="workplace-picture"></div>
+        </div>
+    </div>
+    `;
