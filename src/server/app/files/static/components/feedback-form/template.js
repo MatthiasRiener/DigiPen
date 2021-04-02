e86127f3aeb8df65451fac70fde95fa80ebccdc2
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-04-02 20:38:37 +0200

    feedback-form nearly done

--- src/server/app/files/static/components/feedback-form/template.js
+++ src/server/app/files/static/components/feedback-form/template.js
@@ -1,17 +1,31 @@
 
-  export const template =
+export const template =
     `
-    <div class="workspace-container">
-        <div class="left-side-container">
-            <p id="workspace-title">Create Workspace</p>
-            <p class="textfield-title">Name</p>
-            <input id="workspaceName" type="text" placeholder="Name" />
-            <p class="textfield-title">Invite Users</p>
-            <textarea id="workspaceUsers" class="inv-user-txt" placeholder="Name"></textarea>
-            <div class="btn-create-ws"><p>Create</p></div>
-        </div>
-        <div class="right-side-container">
-            <div class="workplace-picture"></div>
-        </div>
+
+    <div class="outerBox">
+        <h1>Awww.. Tell us what happened</h1>
+        <form action="">
+            <label for="title">Title *</label>
+            <input type="text" id="title" required autocomplete="off" name="title" placeholder="Title of Review...">
+
+            <label for="email">Email *</label>
+            <input type="email" id="email" required autocomplete="off" name="email" placeholder="Enter email...">
+
+            <label for="category">Category *</label>
+            <select id="category" required name="category">
+                <option disabled selected hidden value>Select category ...</option>
+                <option value="Loadingissue">Loadingissue</option>
+                <option value="Display error">Display error</option>
+            </select>
+
+            <label for="description">Description *</label>
+            <textarea id="description" required autocomplete="off" rows="5" cols="50" name="description"
+                placeholder="Describe the problem..."></textarea>
+
+            <p>By submitting this form you agree to our terms and conditions and our Privacy Policy which explains how
+                we may collect, use and disclose your personalinformation including to third parties.</p>
+
+            <input type="submit" value="Submit">
+        </form>
     </div>
     `;
