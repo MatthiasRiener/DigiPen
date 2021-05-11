b5189d72a5ec7d694bf4b790f63522bb007e8120
MatthiasRiener <matti.riener@gmail.com>
2021-05-11 10:37:14 +0200

    little wiggle effect

--- src/server/app/files/static/landing_page/js/eventhandler.js
+++ src/server/app/files/static/landing_page/js/eventhandler.js
@@ -12,3 +12,82 @@ containers.eq(1).append(`<img src="https://images.unsplash.com/photo-14894247310
 containers.eq(1).append(`<img src="https://images.unsplash.com/photo-1520998116484-6eeb2f72b5b9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />`);
 
 containers.eq(2).append(`<img src="https://images.unsplash.com/photo-1541271696563-3be2f555fc4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA5fHxwb3J0cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />`);
+
+
+console.log("was geht ab!")
+
+
+const firstSection = document.getElementById('first-section-intro');
+
+var mouse = {
+    _x: 0,
+    _y: 0,
+    x: 0,
+    y: 0,
+    updatePosition: function (event) {
+        var e = event || window.event;
+        this.x = e.clientX - this._x;
+        this.y = (e.clientY - this._y) * -1;
+    },
+    setOrigin: function (e) {
+        this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+        this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show: function () {
+        return "(" + this.x + ", " + this.y + ")";
+    }
+};
+
+
+var onMouseEnterHandler = function (event) {
+    if (isTimeToUpdate()) {
+        update(event);
+    }
+};
+var onMouseLeaveHandler = function () {
+    stopUpdating();
+};
+var onMouseMoveHandler = function (event) {
+    if (isTimeToUpdate()) {
+        update(event);
+    }
+};
+
+var counter = 0;
+var updateRate = 10;
+var isTimeToUpdate = function () {
+    return counter++ % updateRate === 0;
+};
+
+
+var update = function (event) {
+    mouse.updatePosition(event);
+
+    var companies = document.getElementById('first-section-intro').querySelectorAll(".company-element");
+    companies.forEach((c) => {
+        updateTransformStyle(
+            (mouse.y / c.offsetHeight / 2).toFixed(2),
+            (mouse.x / c.offsetWidth / 2).toFixed(2),
+            c
+        );
+    })
+};
+
+var updateTransformStyle = function (x, y, company) {
+    var style = "rotateX(" + x * 15 + "deg) rotateY(" + y * 15 + "deg)";
+    company.style.transform = style;
+};
+
+function stopUpdating() {
+    var companies = document.getElementById('first-section-intro').querySelectorAll(".company-element");
+    companies.forEach((c) => {
+        var style = "rotateX(" + 0 + "deg) rotateY(" + 0 + "deg)";
+        c.style.transform = style;
+    })
+}
+
+mouse.setOrigin(firstSection);
+
+firstSection.onmouseenter = onMouseEnterHandler;
+firstSection.onmouseleave = onMouseLeaveHandler;
+firstSection.onmousemove = onMouseMoveHandler;
