23b68bc13ce717e53d720ae3c1cd782e3e89376c
LukasFriesenecker <friesilukas@gmail.com>
2021-05-12 15:38:19 +0200

    profile

--- src/server/app/files/static/profile/js/main.js
+++ src/server/app/files/static/profile/js/main.js
@@ -1,11 +1,4 @@
-$('body').on('click', '.pp_bt_logo_click', function () {
-    window.location = "../../../src/landing_page/index.html"
-})
-
-
-$('body').on('click', '.Task', function() {
-    window.location = "/task";
-})
+/*
 
 // dropzone test
 
@@ -38,5 +31,4 @@ $('#imgupload').on('change', function(evt) {
 
     reader.readAsDataURL(files[0])
 })
-
-
+*/
