eda660dc5c7978495545df73967648913e7430ef
MatthiasRiener <matti.riener@gmail.com>
2021-05-30 08:25:01 +0200

    added navigation to landing page

--- src/server/app/files/static/landing_page/js/eventhandler.js
+++ src/server/app/files/static/landing_page/js/eventhandler.js
@@ -18,126 +18,6 @@ containers.eq(1).append(`<img src="https://images.unsplash.com/photo-15209981164
 containers.eq(2).append(`<img src="https://images.unsplash.com/photo-1541271696563-3be2f555fc4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA5fHxwb3J0cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />`);
 
 
-console.log("was geht ab!")
-
-
-const firstSection = document.getElementById('first-section-intro');
-
-var mouse = {
-    _x: 0,
-    _y: 0,
-    x: 0,
-    y: 0,
-    updatePosition: function (event) {
-        var e = event || window.event;
-        this.x = e.clientX - this._x;
-        this.y = (e.clientY - this._y) * -1;
-    },
-    setOrigin: function (e) {
-        this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
-        this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
-    },
-    show: function () {
-        return "(" + this.x + ", " + this.y + ")";
-    }
-};
-
-
-var onMouseEnterHandler = function (event) {
-    if (isTimeToUpdate()) {
-        update(event);
-    }
-};
-var onMouseLeaveHandler = function () {
-    stopUpdating();
-};
-var onMouseMoveHandler = function (event) {
-    if (isTimeToUpdate()) {
-        update(event);
-    }
-};
-
-var counter = 0;
-var updateRate = 10;
-var isTimeToUpdate = function () {
-    return counter++ % updateRate === 0;
-};
-
-
-var update = function (event) {
-    mouse.updatePosition(event);
-
-    var companies = document.getElementById('first-section-intro').querySelectorAll(".company-element");
-    companies.forEach((c) => {
-        updateTransformStyle(
-            (mouse.y / c.offsetHeight / 2).toFixed(2),
-            (mouse.x / c.offsetWidth / 2).toFixed(2),
-            c
-        );
-    })
-};
-
-var updateTransformStyle = function (x, y, company) {
-    var style = "rotateX(" + x * 15 + "deg) rotateY(" + y * 15 + "deg)";
-    company.style.transform = style;
-};
-
-function stopUpdating() {
-    var companies = document.getElementById('first-section-intro').querySelectorAll(".company-element");
-    companies.forEach((c) => {
-        var style = "rotateX(" + 0 + "deg) rotateY(" + 0 + "deg)";
-        c.style.transform = style;
-    })
-}
-
-mouse.setOrigin(firstSection);
-
-firstSection.onmouseenter = onMouseEnterHandler;
-firstSection.onmouseleave = onMouseLeaveHandler;
-firstSection.onmousemove = onMouseMoveHandler;
-
-
-
-
-const SMALL_DEVICE = 1050;
-
-$(document).ready(function () {
-    var width = $(window).width();
-
-    if (width < SMALL_DEVICE) {
-        $('.company-element').css("visibility", "hidden");
-    } else {
-        $('.company-element').css("visibility", "visible");
-    }
-})
-
-$(window).resize(function () {
-    var width = $(window).width();
-
-    if (width < SMALL_DEVICE) {
-        $('.company-element').css("visibility", "hidden");
-    } else {
-        $('.company-element').css("visibility", "visible");
-    }
-})
-
-
-
-$('.faq_question').click(function () {
-
-    if ($(this).parent().is('.open')) {
-        $(this).closest('.faq').find('.faq_answer_container').slideUp();
-        $(this).closest('.faq').removeClass('open');
-    } else {
-        $('.faq_answer_container').slideUp();
-        $('.faq').removeClass('open');
-        $(this).closest('.faq').find('.faq_answer_container').slideDown();
-        $(this).closest('.faq').addClass('open');
-    }
-
-});
-
-
 
 // 
 
@@ -308,3 +188,39 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+
+$('#top-bar-subpoints p').click(function() {
+    const el = $(this).index();
+    $('#top-bar-subpoints p').removeClass("active");
+    $('#top-bar-subpoints p').eq(el).addClass("active");
+    console.log(el, "was clicked!")
+});
+
+
+$('#support-nav-point').click(() => {
+    goToByScroll("map-section")
+});
+
+
+$('#pricing-nav-point').click(() => {
+    goToByScroll("section-pricing")
+});
+
+
+$('#features-nav-point').click(() => {
+    goToByScroll("section-special")
+});
+
+
+$('#customers-nav-point').click(() => {
+    goToByScroll("section-customers")
+});
+
+
+function goToByScroll(id){
+    // Reove "link" from the ID
+    // Scroll
+  $('html,body').animate({
+      scrollTop: $("#" + id).offset().top - window.innerHeight / 100 * 5},
+      'slow');
+}
