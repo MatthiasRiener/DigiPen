aee3e71d99380db1e49bd637d94a3a660d6a0cd8
MatthiasRiener <matti.riener@gmail.com>
2021-05-30 09:44:40 +0200

    added sound

--- src/server/app/files/static/editor/js/audio-player.js
+++ src/server/app/files/static/editor/js/audio-player.js
@@ -6,3 +6,8 @@ function audioSelfDisonnected() {
     var audio = new Audio(baseURL + "/static/editor/sounds/selfDisconnect.mp3");
     audio.play();
 }
+
+function audioUserJoined() {
+    var audio = new Audio(baseURL + "/static/editor/sounds/IMPORTANT_404.mp3");
+    audio.play();
+}
