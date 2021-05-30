9b40cb31abc7ce876a894cdfa34e2d1a21a9789d
MatthiasRiener <matti.riener@gmail.com>
2021-05-30 10:44:41 +0200

    new soudns

--- src/server/app/files/static/editor/js/audio-player.js
+++ src/server/app/files/static/editor/js/audio-player.js
@@ -1,3 +1,5 @@
+var joinSound;
+
 function audioSelfConnected() {
     
 }
@@ -8,6 +10,17 @@ function audioSelfDisonnected() {
 }
 
 function audioUserJoined() {
+    joinSound = new Audio(baseURL + "/static/editor/sounds/amoung.mp3");
+    joinSound.play();
+}
+
+function selfJoined() {
     var audio = new Audio(baseURL + "/static/editor/sounds/IMPORTANT_404.mp3");
     audio.play();
+}
+
+
+function dismissCall() {
+    joinSound.stop();
+    joinSound.currentTime = 0;
 }
